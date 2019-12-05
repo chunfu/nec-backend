4628e77947ff2efd0c4c3b4c5af8bdb3b3d60eca
chunfu <hideoryan@gmail.com>
2019-12-05 19:22:50 +0800

    import futil

--- src/api/pos/sla.js
+++ src/api/pos/sla.js
@@ -3,11 +3,7 @@ import * as util from 'util';
 import { exec } from 'child_process';
 const execAsync = util.promisify(exec);
 import { excel2json } from '../../lib/util';
-import {
-  MOVETIME_FILE_PATH,
-  NEED_ADJUST_PATH,
-  NEED_ADJUST_OK_PATH,
-} from '../../lib/files';
+import * as futil from '../../lib/files';
 
 // material-table will pollute original data with additional tableData property
 // we have to remove it before export to needAdjustOK.xlsx
@@ -33,9 +29,9 @@ const getSla = async (req, res) => {
   // sla.py should export sla.xlsx as result
   try {
     const { stdout, stderr } = await execAsync(
-      `cd ${futil.projectRoot}/modules && python -c "import SLA; SLA.SLAcheck(${serviceQuality}, '${MOVETIME_FILE_PATH}')"`,
+      `cd ${futil.projectRoot}/modules && python -c "import SLA; SLA.SLAcheck(${serviceQuality}, '${futil.MOVETIME_FILE_PATH}')"`,
     );
-    const [rows] = excel2json(NEED_ADJUST_PATH);
+    const [rows] = excel2json(futil.NEED_ADJUST_PATH);
     const columns =
       rows.length &&
       Object.keys(rows[0]).map(key => ({ title: key, field: key }));
@@ -57,7 +53,7 @@ const putSla = async (req, res) => {
       header: columns.map(c => c.field),
     });
     workbook.Sheets['sheet1'] = worksheet;
-    xlsx.writeFile(workbook, NEED_ADJUST_OK_PATH);
+    xlsx.writeFile(workbook, futil.NEED_ADJUST_OK_PATH);
 
     res.json({ columns, rows });
   } catch (e) {
